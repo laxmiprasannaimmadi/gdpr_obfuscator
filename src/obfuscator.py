6c59e4034b44435b8bcbdf35bf1a04dd352e1210
laxmiprasannaimmadi <laxmiprasanna.immadi@gmail.com>
2024-12-03 17:39:28 +0000

    getting s3 bucket and file details

--- src/obfuscator.py
+++ src/obfuscator.py
@@ -1,5 +1,26 @@
+from urllib.parse import urlparse
+import logging
+
 def obfuscator(file_path):
     '''
-    Retrieve data from AWS S3 
+    Retrieve data being ingested to AWS and intercept personally identifiable information (PII).
+
+    Return byte-stream object containing an exact copy of the input file but with the sensitive data replaced with obfuscated strings.
+
     '''
+    logger = logging.getLogger("Obfuscation process")
+    logging.basicConfig()
+    logger.setLevel(logging.INFO)
+
+    logger.info("GDPR Obfuscation process started")
     pass
+
+def get_bucket_key(s3_file_path):
+    '''
+    Extract S3 bucket name and key of the object from s3 file path
+    Example: s3 file path: 's3://my_ingestion_bucket/new_data/file1.csv'    
+    '''
+
+    o = urlparse(s3_file_path, allow_fragments=False)
+    return o.netloc, o.path.lstrip('/')
+
